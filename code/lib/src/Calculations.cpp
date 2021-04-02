150dc92b78cf6c94f47b72187cc36aa748749d62
Victor Gueorguiev <victor@amplifyanalytix.com>
2021-04-03 01:49:14 +0300

    Bug fixing multithreading producer and consumer v9.

--- code/lib/src/Calculations.cpp
+++ code/lib/src/Calculations.cpp
@@ -52,12 +52,13 @@ tuple<const double, const Question> Calculations::find_best_split(const Data& ro
 		ClassCounter candidateTrueCounts = overall_counts;
 		ClassCounter candidateFalseCounts = overall_counts;
 		tuple<std::string, double> bestThreshAndLoss;
+		std::cout << "Whoop whoop0" << std::endl;
 		if (colType.compare("categorical") == 0) {
 			std::tie(candidateThresh, candidateLoss, candidateTrueSize, candidateFalseSize, candidateTrueCounts, candidateFalseCounts) = determine_best_threshold_cat(rows, column);
 		} else {
 			std::tie(candidateThresh, candidateLoss, candidateTrueSize, candidateFalseSize, candidateTrueCounts, candidateFalseCounts) = determine_best_threshold_numeric(rows, column);
 		}
-		std::cout << "Whoop whoop" << std::endl
+		std::cout << "Whoop whoop1" << std::endl;
 
 		// = bestThreshAndLoss;
 		if (candidateTrueSize == 0 || candidateFalseSize == 0)
@@ -66,7 +67,7 @@ tuple<const double, const Question> Calculations::find_best_split(const Data& ro
 		
 
 		const auto &candidateGain = info_gain(candidateTrueCounts, candidateFalseCounts, candidateTrueSize, candidateFalseSize, current_uncertainty);
-
+		std::cout << "Whoop whoop2" << std::endl;
 		#pragma omp critical
 		{
 			if (candidateGain >= bestGain) {
@@ -75,6 +76,7 @@ tuple<const double, const Question> Calculations::find_best_split(const Data& ro
 				bestQuestion = q;
 			}
 		}
+		std::cout << "Whoop whoop3" << std::endl;
 	}
 
   return forward_as_tuple(bestGain, bestQuestion);
@@ -154,7 +156,7 @@ std::tuple<std::string, double, double, double, ClassCounter, ClassCounter> Calc
 					currentFeatureValue = row.at(0);
         }
   }
-		
+	std::cout << "Whoop whoop6" << std::endl;	
   return forward_as_tuple(bestThresh, bestLoss, bestTrueSize, bestFalseSize, bestTrueCounts, bestFalseCounts);
 }
 
@@ -223,7 +225,7 @@ std::tuple<std::string, double, double, double, ClassCounter, ClassCounter> Calc
 						break;
 		}
 	}
-	
+	std::cout << "Whoop whoop5" << std::endl;
   return forward_as_tuple(bestThresh, bestLoss, bestTrueSize, bestFalseSize, bestTrueCounts, bestFalseCounts);
 }
 
